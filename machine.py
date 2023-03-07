de1501c50a70dcd4aa8472f6f5c833dfbd9a850e
Jessica Chen <jessicachen@dhcp-10-250-156-164.harvard.edu>
2023-03-07 12:17:48 -0500

    logging and clocks work on python3.7

--- machine.py
+++ machine.py
@@ -1,4 +1,3 @@
-
 import random
 import os
 from _thread import *
@@ -10,7 +9,7 @@ from multiprocessing import Process
 
 class Machine:
 
-    def __init__(self, config):
+    def __init__(self, config, number):
 
         config.append(os.getpid()) # get current process id
 
@@ -18,9 +17,16 @@ class Machine:
         self.host = str(config[0])
         self.port = int(config[1])
         self.config = config
-        self.clock = 0
+        self.clock = [0,0,0]
+        self.number = number # id for the machine
+        self.queue = [] # network queue
 
-        self.queue = queue.SimpleQueue # network queue
+        if self.number == 0:
+            self.file = "m0.txt"
+        elif self.number == 1:
+            self.file = "m1.txt"
+        else:
+            self.file = "m2.txt"
 
 
     def init_machine(self): # receive function, essentially
@@ -35,35 +41,22 @@ class Machine:
 
         while True:
             conn, _ = s.accept() # all must connect first, then start receiving
-            start_new_thread(self.consumer, (conn,)) # consumer reads, decodes msgs and add to queue
+            start_new_thread(self.consumer, (conn,))
     
     def consumer(self, conn): # called when starting new thread
         # conn is client connection received after s.accept(); can use conn.send()
         print("consumer accepted connection" + str(conn)+"\n")
 
         while True: # receiving messages to queue NOT constrained to rate
+            data = conn.recv(1024).decode('ascii')
+            print(self.number, " queue appended with: ", data)
+            self.queue.append(data) # network queue
 
-            data = conn.recv(1024) # blocks until connection from client
-            print("msg received\n")
-
-            # decode 
-            dataVal = data.decode('ascii')
-            print("msg received:", dataVal)
-
-            # network queue
-            self.queue.put(dataVal)
-
-    def start(self):
-        # init listening thread on home port
-        init_thread = Thread(target=self.init_machine)
-        init_thread.start()
-
-        #add delay to initialize the server-side logic on all processes
-        time.sleep(5)
-
+    def producer(self):
         # producers "receive" messages by connecting sockets to other ports
         self.prod1 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.port1 = int(self.config[2])
+
         try:
             self.prod1.connect((self.host, self.port1))
             print("Client-side connection success to port val:" + str(self.port1) + "\n")
@@ -72,50 +65,73 @@ class Machine:
 
         self.prod2 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         self.port2 = int(self.config[3])
+
         try:
             self.prod2.connect((self.host, self.port2))
             print("Client-side connection success to port val:" + str(self.port2) + "\n")
         except socket.error as e:
             print ("Error connecting producer: %s" % e)
 
-        while True:
-            time.sleep(self.rate)
-            if self.queue().qsize() > 0:
-                msg = self.queue.get()
-                self.clock += 1
-                # write to log: received, global time, length of queue, logical clock time
-            else:
-                rng = random.randint(1,10)
-                if rng == 1: # send to one of other machines
-                    msg = str(self.clock)
-                    self.prod1.send(msg.encode('ascii'))
-                    print("msg sent", msg)
-                    self.clock += 1
+        with open(self.file, "w") as f:
+            self.opened_file = f
+            f.write("Opened\n")
+            start = time.time()
+            while time.time() - start < 5:
+                time.sleep(self.rate)
+                if len(self.queue) > 0:
+                    msg = self.queue.pop()
+                    data_list = msg.split("|")
+                    for i in range(0,3):
+                        self.clock[i] = max(int(data_list[i]), self.clock[i]) + 1
+                    print(self.number, " received: ", self.clock)
+                    f.write("Received "+ msg+ "|"+str(time.time())+"|"+str(len(self.queue))+"|"+str(self.clock)+"\n")
+                    # write to log: received, global time, length of queue, logical clock time
+                else:
+                    status = ''.join(str(n)+"|" for n in self.clock)
+                    rng = random.randint(1,10)
+                    if rng == 1: # send to one of other machines
+                        self.prod1.send(status.encode('ascii'))
+                        print("status sent", status)
+                        action = "Sent"
+                        # write log with send, system time, logical clock time
+                    elif rng == 2: # send to other machine
+                        # send to one of other machines
+                        self.prod2.send(status.encode('ascii'))
+                        print("status sent", status)
+                        action = "Sent"
+                        # write log with send, system time, logical clock time
+                    elif rng == 3: # send to both other machines
+                        # send to one of other machines
+                        self.prod1.send(status.encode('ascii'))
+                        self.prod2.send(status.encode('ascii'))
+                        print("status sent", status)
+                        action = "Sent"
+                        # write log with send, system time, logical clock time
+                    else:
+                        print(str(self.number) + " resting")
+                        # log internal event, system time, logical clock value
+                        action = "Internal"
+                    self.clock[self.number] += 1
+                    f.write(action+"|"+str(time.time())+"|"+str(self.clock)+"\n")
+        f.close()
+            
+    def start(self):
+        # init listening thread on home port
+        init_thread = Thread(target=self.init_machine)
+        init_thread.start()
 
-                    # write log with send, system time, logical clock time
-                elif rng == 2: # send to other machine
-                    # send to one of other machines
-                    msg = str(self.clock)
-                    self.prod2.send(msg.encode('ascii'))
-                    print("msg sent", msg)
-                    self.clock += 1
+        #add delay to initialize the server-side logic on all processes
+        time.sleep(5)
 
-                    # write log with send, system time, logical clock time
-                elif rng == 3: # send to both other machines
-                    # send to one of other machines
-                    msg = str(self.clock)
-                    self.prod1.send(msg.encode('ascii'))
-                    self.prod2.send(msg.encode('ascii'))
-                    print("msg sent", msg)
-                    self.clock += 1
+        prod_thread = Thread(target=self.producer)
+        prod_thread.start()
 
-                    # write log with send, system time, logical clock time
-                else: # internal event
-                    self.clock += 1
-                    # log internal event, system time, logical clock value
-                
+    def close_file(self):
+        self.opened_file.close()
+
 
 if __name__ == '__main__':
+    
     localHost= "127.0.0.1"
 
     port1 = 2056
@@ -123,20 +139,30 @@ if __name__ == '__main__':
     port3 = 4056
 
     config1=[localHost, port1, port2, port3]
-    m1 = Machine(config1)
+    m0 = Machine(config1, 0)
     config2=[localHost, port2, port1, port3]
-    m2 = Machine(config2)
-    config3=[localHost, port3, port1, port2]
-    m3 = Machine(config3)    
+    m1 = Machine(config2, 1)
+    config3=[localHost, port3, port2, port1]
+    m2 = Machine(config3, 2)    
     
-    p1 = Process(target=m1.start)
-    p2 = Process(target=m2.start)
-    p3 = Process(target=m3.start)
+    try:
+        p1 = Process(target=m0.start)
+        p2 = Process(target=m1.start)
+        p3 = Process(target=m2.start)
 
-    p1.start()
-    p2.start()
-    p3.start()
+        p1.start()
+        p2.start()
+        p3.start()
 
-    p1.join()
-    p2.join()
-    p3.join()
+        p1.join()
+        p2.join()
+        p3.join()
+
+
+    except KeyboardInterrupt:
+        # m0.close_file()
+        # m1.close_file()
+        # m2.close_file()
+        p1.terminate()
+        p2.terminate()
+        p3.terminate()
